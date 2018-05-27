a6f60ac3fe95b4cfe6e3c9d9154e2d3dabafa30c
Harsh Mishra <34878554+GhosTmaNHarsh@users.noreply.github.com>
2018-05-28 01:03:18 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -1,52 +1,57 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-clear
-echo "|-------------------------------------|"
-echo "|        I Am Harsh Mishra.           |"
-echo "|                                     |"
-echo "|   Subscribe to YouTube Channel      |"
-echo "|           Harsh ki vani             |"
-echo "|-------------------------------------|"
-echo "|                                     |"
-echo "|            Ghostman(+_+)            |"
-echo "|                                     |"
-echo "|+++++++++++++++++++++++++++++++++++++|"
-echo "|       Metasploit  Framework         |"
-echo "|+++++++++++++++++++++++++++++++++++++|"
-echo "|_____________________________________|" 
+apt install pv -y
+pkg install ruby -y
+gem install lolcat
+	clear
+	echo " "
+	echo " "
+	echo "    Author          :  Harsh Mishra "
+	sleep 1
+	echo " "
+	echo "    YouTube Channel :  Harsh ki vani"
+	echo " "
+	sleep 1
+	echo "  Guys just be supporting me and motivating " | lolcat
+ 	echo "  me for more new tools and much more things " | lolcat
+	echo " "
+	echo " "
+	echo "		           - Thank you to all" | lolcat
+	echo "	          	      my subscribers" | lolcat
+	echo " "
+	sleep 1
+	echo " "
+	echo -n " press enter to continue........." | lolcat
+	read FA
 
 
-echo "WAIT UNTIL INSTALLING............" 
+apt install -y autoconf bison clang coreutils axel curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner
 
-echo "-----------------------------------------"
-apt install -y autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config postgresql-contrib wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner
-echo "-----------------------------------------"
+echo "=======================================" | pv -qL 20
+
 apt update && apt upgrade
-echo "Downloading & Extracting....."
+echo " [ + ] Downloading & Extracting....... " |  pv -qL 20
+
+find $HOME -name "*.16.tar.gz" -type f
+
+# Remove Old Folders
+find $HOME -name "metasploit*" -type d
 
 cd $HOME
-curl -LO https://github.com/rapid7/metasploit-framework/archive/4.16.16.tar.gz
-tar -xf $HOME/4.16.16.tar.gz
+axel --alternate https://github.com/rapid7/metasploit-framework/archive/4.16.16.tar.gz
+tar -xf $HOME/metasploit-framework-4.16.16.tar.gz
 mv $HOME/metasploit-framework-4.16.16 $HOME/metasploit-framework
-rm $HOME/4.16.16.tar.gz
+rm $HOME/metasploit-framework-4.16.16.tar.gz
 cd $HOME/metasploit-framework
 sed '/rbnacl/d' -i Gemfile.lock
 sed '/rbnacl/d' -i metasploit-framework.gemspec
 gem install bundler
 sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+ln -sf $PREFIX/include/libxml2/libxml $PREFIX/include/
 
 gem install nokogiri -v 1.8.0 -- --use-system-libraries
  
 #sed 's|grpc (.*|grpc (1.4.1)|g' -i $HOME/metasploit-framework/Gemfile.lock
-#gem unpack grpc -v 1.4.1
-#cd grpc-1.4.1
-#curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec
-#curl -L https://raw.githubusercontent.com/Hax4us/Hax4us.github.io/master/extconf.patch
-#patch -p1 < extconf.patch
-#gem build grpc.gemspec
-#gem install grpc-1.4.1.gem
-#cd ..
-#rm -r grpc-1.4.1
 
 
 cd $HOME/metasploit-framework
@@ -59,11 +64,15 @@ fi
 if [ -e $PREFIX/bin/msfvenom ];then
 	rm $PREFIX/bin/msfvenom
 fi
-ln -s $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
-ln -s $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+ln -sf $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
+ln -sf $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
 termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.4.0/gems/pg-0.20.0/lib/pg_ext.so
-echo "Creating database"
-
+clear
+echo " [ $ ] Creating database "
+echo " "
+echo -n " [ Loading"
+echo "..............................]" | pv qL 20
+echo " "
 cd $HOME/metasploit-framework/config
 curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/database.yml
 
@@ -73,21 +82,20 @@ initdb $PREFIX/var/lib/postgresql
 pg_ctl -D $PREFIX/var/lib/postgresql start
 createuser msf
 createdb msf_database
-
-echo "®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®"
-echo "Thanx  To  Vishalbiswani"
-echo "®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®"
-
-echo "++++++++++++++++++++++++++++++++++++++++++"
-echo "_____________________________________________"
-echo ".  |-------------------------------------|"
-echo ".  |   INSTALLATION PROCESS COMPLETED    |"
-echo "   |-------------------------------------|"
-echo "        Thank you for installing....... "
-echo "_____________________________________________"
-echo "------------------------------------"
-echo "For  More. Tricks. Visit  At  harshkivani.wordpress.com"
-echo "------------------------------------"
-echo "++++++++++++++++++++++++++++++++++++"
-echo " NOW YOU CAN RUN METASPLOIT BY JUST EXECUTE THE COMMAND => msfconsole"
-echo "++++++++++++++++++++++++++++++++++++"
+clear
+echo "           ___[+ + + + ]____"
+echo " _________[__  GhosTmaN  ___]_________"
+echo " |           [          ]            |"
+echo " | For more information u can visit  |"
+echo " |    to my channel Harsh ki vani    |"
+echo " +-----------------------------------+"
+echo " |         [   (^_^ )/   ]           |"
+echo " |                                   |"
+echo " | Friends just keep motivating me   |"
+echo " | by supporting me.                 |"
+echo " |                                   |"
+echo " +___________________________________+"
+echo " "
+echo " [ $ ] Now you can run metasploit by using " | pv -qL 20
+echo -n "       the commond => " | pv -qL 20
+echo "msfconsole" | lolcat | pv -qL 10
